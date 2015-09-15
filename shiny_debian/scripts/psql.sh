cf761dc890561a53b12a9aa4e38513f5d265bfd1
Jorge Martinez Ortega <j.martinezortega@gmail.com>
2015-09-14 22:01:33 -0500

    psql script to create user and db

---
+++ shiny_debian/scripts/psql.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+pg_ctlcluster `pg_lsclusters| tail -1| awk '{print $1}'` \
+  `pg_lsclusters|tail -1 | awk '{print $2}'` start
+
+su -c "psql -c \"CREATE USER root WITH PASSWORD '$POSTGRES_PASSWD';\"" \
+    -s /bin/bash postgres
+
+su -c "psql -c \"CREATE DATABASE tomala OWNER root;\"" \
+    -s /bin/bash postgres
+
+    pg_ctlcluster `pg_lsclusters| tail -1| awk '{print $1}'` \
+      `pg_lsclusters|tail -1 | awk '{print $2}'` stop
